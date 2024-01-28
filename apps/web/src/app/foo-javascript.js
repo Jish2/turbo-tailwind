9d4df55ca147fc16189751a0f76161148358aacc
Magnus <magnus.rinnan@gmail.com>
2024-01-28 14:08:59 +0000

    foo

--- apps/web/src/app/foo-javascript.js
+++ apps/web/src/app/foo-javascript.js
@@ -1,7 +1,8 @@
 // Does not find the package when hitting ctrl+click.
 // This works if importing into a TypeScript file (see foo-typescript.tsx)
-import { Card } from '@repo/ui/'
+import { Card } from '@repo/ui';
+import { Bar } from '@repo/ui/bar'
 
 export function Foo() {
-  return <Card >acme</Card>;
+  return <Card ><Bar /></Card>;
 }
